2c0a66d7871c9b426e1a54dc6d829ef97e6eff8d
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2010-12-28 15:27:37 -0300

    Programa3 migrado para python3

---
+++ programa3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+programa3.py
+
+Created by Pedro Brasileiro Cardoso Junior on 2010-12-28.
+Copyright (c) 2010 Particular. All rights reserved.
+
+Exemplo com comentários
+
+"""
+print() # Imprime uma linha em branco
+
+# Imprime outra linha em branco
+print()
+
+valor = ""
+if valor != "olá":
+  print(valor)
+# Comando de debug
+  print("DEBUG")
+  """
+  Comentários com mais de uma linha
+  precisam ficar dentro de uma 
+  string multilinha.
+  """
